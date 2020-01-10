2776ca46c4a9d928450e3ac773c880b85f0d826c
Irish Fagan <IrishFagan@live.com>
2020-01-10 09:33:37 -0800

    Add show_help function and functionality. Also include details for show_help

--- scp_osu.bash
+++ scp_osu.bash
@@ -5,6 +5,27 @@ SRC=0
 OPTION=0
 HELP=0
 
+show_help()
+{
+	echo "./scp_osu.bash [-h] [-s <source>] [-d <destination>] [-o <optional>] [-r]"
+	echo "	"
+	echo "---"
+	echo "	"
+	echo "[-h] - Show help"
+	echo "	"
+	echo "[-d] - Set destination directory"
+	echo "       Ex: ~/dog/destination"
+	echo "	"
+	echo "[-o] - Set optional server to access other than flip"
+	echo "       Ex: os1, os2"
+	echo "	"
+	echo "[-r] - Recursively download all files and subdirectories within a directory"
+	echo "	"
+	echo "[-s] - Set source directory or file"
+	echo "       Ex: ~/cat/file.exe, ~/cat"
+	echo "       NOTE: -r must be specified in order to download a directory"
+}
+
 while getopts "hd:s:o:" OPT
 do
 	case "${OPT}" in
@@ -22,3 +43,8 @@ do
 			;;
 	esac
 done
+
+if [ $HELP=1 ]
+then
+	show_help
+fi
