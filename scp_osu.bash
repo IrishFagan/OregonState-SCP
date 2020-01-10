6c59284ed6baa6504cdde349769f24579ba0edeb
Irish Fagan <IrishFagan@live.com>
2020-01-10 09:52:00 -0800

    Add user variable and functionality to case statement Add more details to show_help function about user argument

--- scp_osu.bash
+++ scp_osu.bash
@@ -5,10 +5,11 @@ SRC=0
 OPTION=0
 HELP=0
 RECURSE=0
+USER=0
 
 show_help()
 {
-	echo "./scp_osu.bash [-h] [-s <source>] [-d <destination>] [-o <optional>] [-r]"
+	echo "./scp_osu.bash [-h] [-s <source>] [-d <destination>] [-o <optional>] [-r] [-u]"
 	echo "	"
 	echo "---"
 	echo "	"
@@ -25,9 +26,14 @@ show_help()
 	echo "[-s] - Set source directory or file"
 	echo "       Ex: ~/cat/file.exe, ~/cat"
 	echo "       NOTE: -r must be specified in order to download a directory"
+	echo "	"
+	echo "[-u] - Set user"
+	echo "       Ex: washingtong"
+	echo "       NOTE: -u must be specified or the USER variable must be changed"
+	exit 0
 }
 
-while getopts "hd:s:o:" OPT
+while getopts "hd:s:o:ru:" OPT
 do
 	case "${OPT}" in
 		h)
@@ -44,6 +50,9 @@ do
 			;;
 		r)
 			RECURSE=1
+			;;
+		u)
+			USER="${OPTARG}"
 			;;
 	esac
 done
